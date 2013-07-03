c6e510f6c8377e543cb9b1703a446bd20fab2f7f
Alexandre Joly <alexandre.joly@futurecom.ch>
2013-07-03 10:26:54 +0200

    merge with master

--- travis/setup.sh
+++ travis/setup.sh
@@ -9,19 +9,19 @@ cd /tmp
 git clone https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} gh-pages 2>&1
 cd gh-pages
 
-echo '---- Switch to gh-pages branch ----'
-git checkout gh-pages
-
 echo '---- Set git settings ----'
 git config --global user.name $GIT_AUTHOR_NAME
 git config --global user.email $GIT_AUTHOR_EMAIL
 git config --global push.default matching
 
-echo '---- Rebase gh-pages ----'
+echo '---- Switch to gh-pages branch ----'
+git checkout gh-pages
+
+echo '---- Merge master to gh-pages ----'
 git rebase master
 
 echo '---- Push the changes ----'
-git push --mirror https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} 2>&1
+git push https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} 2>&1
 
 echo '######################################'
 echo '#                SETUP               #'
